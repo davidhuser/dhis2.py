6650c6df12071351107398770ff8d3d6f497ea44
dhuser <dhuser@baosystems.com>
2018-06-14 01:55:26 +0200

    paging tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,6 +10,11 @@ BASEURL = 'https://play.dhis2.org/2.29'
 API_URL = '{}/api'.format(BASEURL)
 
 
+@pytest.fixture
+def api():
+    return Dhis(BASEURL, 'admin', 'district')
+
+
 @pytest.mark.parametrize("status_code", [
     400, 401, 402, 403, 404, 405, 406, 407, 408, 409,
     410, 411, 412, 413, 414, 415, 416, 417, 418, 421,
@@ -18,12 +23,11 @@ API_URL = '{}/api'.format(BASEURL)
     506, 507, 508, 510, 511
 ])
 @responses.activate
-def test_client_server_errors(status_code):
+def test_client_server_errors(api, status_code):
     url = '{}/dataElements/foo.json'.format(API_URL)
 
     responses.add(responses.GET, url, body='something failed', status=status_code)
 
-    api = Dhis(BASEURL, 'admin', 'district')
     with pytest.raises(exceptions.APIException) as e:
         api.get(endpoint='dataElements/foo')
     assert e.value.code == status_code
@@ -37,13 +41,12 @@ def test_client_server_errors(status_code):
 
 
 @responses.activate
-def test_post():
+def test_post(api):
     url = '{}/metadata'.format(API_URL)
     p = {"obj": "some data"}
 
     responses.add(responses.POST, url, json=p, status=201)
 
-    api = Dhis(BASEURL, 'admin', 'district')
     api.post(endpoint='metadata', data=p)
 
     assert len(responses.calls) == 1
@@ -51,13 +54,12 @@ def test_post():
 
 
 @responses.activate
-def test_put():
+def test_put(api):
     url = '{}/organisationUnits/uid'.format(API_URL)
     p = {"obj": "some data"}
 
     responses.add(responses.PUT, url, json=p, status=200)
 
-    api = Dhis(BASEURL, 'admin', 'district')
     api.put(endpoint='organisationUnits/uid', data=p)
 
     assert len(responses.calls) == 1
@@ -65,13 +67,12 @@ def test_put():
 
 
 @responses.activate
-def test_patch():
+def test_patch(api):
     url = '{}/organisationUnits/uid'.format(API_URL)
     p = {"obj": "some data"}
 
     responses.add(responses.PATCH, url, json=p, status=200)
 
-    api = Dhis(BASEURL, 'admin', 'district')
     api.patch(endpoint='organisationUnits/uid', data=p)
 
     assert len(responses.calls) == 1
@@ -79,12 +80,11 @@ def test_patch():
 
 
 @responses.activate
-def test_delete():
+def test_delete(api):
     url = '{}/organisationUnits/uid'.format(API_URL)
 
     responses.add(responses.DELETE, url, status=200)
 
-    api = Dhis(BASEURL, 'admin', 'district')
     api.delete(endpoint='organisationUnits/uid')
 
     assert len(responses.calls) == 1
@@ -92,13 +92,12 @@ def test_delete():
 
 
 @responses.activate
-def test_info():
+def test_info(api):
     url = '{}/system/info.json'.format(API_URL)
     r = {"contextPath": "https://play.dhis2.org/2.29"}
 
     responses.add(responses.GET, url, json=r, status=200)
 
-    api = Dhis(BASEURL, 'admin', 'district')
     resp = api.info()
 
     assert resp == json.dumps(r, indent=2)
@@ -110,57 +109,61 @@ def test_info():
 @pytest.mark.parametrize("page_size,no_of_pages,expected_amount", [
     [50, 2, 95],
     [100, 2, 195],
+    [200, 3, 595],
     [20, 1, 15]
 ])
 @responses.activate
-def test_get_paged(page_size, no_of_pages, expected_amount):
+def test_get_paged(api, page_size, no_of_pages, expected_amount):
     """
     first page: $page_size
     second page: $page_size minus overflow
     """
     overflow = 5
 
-    if no_of_pages > 1:
-        first_page = {
-            "pager": {
-                "page": 1,
-                "pageCount": no_of_pages,
-                "total": expected_amount,
-                "pageSize": page_size,
-                "nextPage": "{}/organisationUnits?page=2".format(API_URL)
-            },
-            "organisationUnits": [str(uuid.uuid4()) for _ in range(page_size)]
-        }
-        url = '{}/organisationUnits.json?pageSize={}'.format(API_URL, page_size)
-        responses.add(responses.GET, url, json=first_page, status=200)
-
-        second_page_count = page_size - overflow
-        second_page = {
-            "pager": {
-                "page": 2,
-                "pageCount": no_of_pages,
-                "total": page_size + overflow,
-                "pageSize": page_size
-            },
-            "organisationUnits": [str(uuid.uuid4()) for _ in range(second_page_count)]
-        }
-        url = '{}/organisationUnits.json?pageSize={}&page=2'.format(API_URL, page_size)
-        responses.add(responses.GET, url, json=second_page, status=200)
-
+    # one page response
     if no_of_pages == 1:
-        first_page = {
+        single_page = {
             "pager": {
                 "page": 1,
-                "pageCount": no_of_pages,
+                "pageCount": expected_amount,
                 "total": expected_amount,
                 "pageSize": page_size
             },
             "organisationUnits": [str(uuid.uuid4()) for _ in range(expected_amount)]
         }
-        url = '{}/organisationUnits.json?pageSize={}'.format(API_URL, page_size)
-        responses.add(responses.GET, url, json=first_page, status=200)
+        url = '{}/organisationUnits.json?pageSize={}&page=1'.format(API_URL, page_size)
+        responses.add(responses.GET, url, json=single_page, status=200)
 
-    api = Dhis(BASEURL, 'admin', 'district')
+    # multi page response
+    else:
+        for i in range(1, no_of_pages):
+            normal_page = {
+                "pager": {
+                    "page": i,
+                    "pageCount": page_size,
+                    "total": expected_amount,
+                    "pageSize": page_size,
+                    "nextPage": "{}/organisationUnits?page={}".format(API_URL, i+1)
+                },
+                "organisationUnits": [str(uuid.uuid4()) for _ in range(page_size)]
+            }
+            url = '{}/organisationUnits.json?pageSize={}'.format(API_URL, page_size)
+            responses.add(responses.GET, url, json=normal_page, status=200)
+
+        # last page of multi page response
+        last_amount = page_size - overflow
+        last_page = {
+            "pager": {
+                "page": no_of_pages,
+                "pageCount": last_amount,
+                "total": expected_amount,
+                "pageSize": page_size
+            },
+            "organisationUnits": [str(uuid.uuid4()) for _ in range(last_amount)]
+        }
+        url = '{}/organisationUnits.json?pageSize={}&page={}'.format(API_URL, page_size, no_of_pages)
+        responses.add(responses.GET, url, json=last_page, status=200)
+
     counter = 0
     uid_list = []
     for page in api.get_paged('organisationUnits', page_size=page_size):
@@ -172,7 +175,7 @@ def test_get_paged(page_size, no_of_pages, expected_amount):
 
 
 @responses.activate
-def test_get_paged_empty():
+def test_get_paged_empty(api):
     page_size = 50
 
     first_page = {
@@ -184,10 +187,9 @@ def test_get_paged_empty():
         },
         "organisationUnits": []
     }
-    url = '{}/organisationUnits.json?pageSize={}'.format(API_URL, page_size)
+    url = '{}/organisationUnits.json?pageSize={}&page=1'.format(API_URL, page_size)
     responses.add(responses.GET, url, json=first_page, status=200)
 
-    api = Dhis(BASEURL, 'admin', 'district')
     counter = 0
     for page in api.get_paged('organisationUnits', page_size=50):
         counter += len(page['organisationUnits'])
@@ -196,16 +198,14 @@ def test_get_paged_empty():
 
 
 @responses.activate
-def test_page_size_zero():
-    api = Dhis(BASEURL, 'admin', 'district')
+def test_page_size_zero(api):
     with pytest.raises(exceptions.ClientException):
         for _ in api.get_paged('organisationUnits', page_size=0):
             pass
 
 
 @responses.activate
-def test_paging_with_params():
-    api = Dhis(BASEURL, 'admin', 'district')
+def test_paging_with_params(api):
     url = '{}/organisationUnits.json?pageSize=50&paging=False'.format(API_URL)
     r = {
         "pager": {
@@ -229,13 +229,12 @@ def test_paging_with_params():
     ("2.30-SNAPSHOT", 30)
 ])
 @responses.activate
-def test_dhis_version(from_server, integer):
+def test_dhis_version(api, from_server, integer):
     url = '{}/system/info.json'.format(API_URL)
     r = {"version": from_server}
 
     responses.add(responses.GET, url, json=r, status=200)
 
-    api = Dhis(BASEURL, 'admin', 'district')
     resp = api.dhis_version()
 
     assert resp == integer
@@ -245,13 +244,12 @@ def test_dhis_version(from_server, integer):
 
 
 @responses.activate
-def test_dhis_version_invalid():
+def test_dhis_version_invalid(api):
     url = '{}/system/info.json'.format(API_URL)
     r = {"version": "unknown"}
 
     responses.add(responses.GET, url, json=r, status=200)
 
-    api = Dhis(BASEURL, 'admin', 'district')
     with pytest.raises(exceptions.ClientException):
         api.dhis_version()
 
@@ -268,11 +266,10 @@ def test_chunk(amount, expected):
 
 
 @responses.activate
-def test_generate_uids():
+def test_generate_uids(api):
     amount = 13000
     url = '{}/system/id.json'.format(API_URL, amount)
 
     responses.add_passthru(url)
-    api = Dhis(BASEURL, 'admin', 'district')
     uids = api.generate_uids(amount)
     assert (len(uids) == amount)
