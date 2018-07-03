18416c056aa638ed2a4fc1d6c930ce0f0abac6af
dhuser <dhuser@baosystems.com>
2018-07-04 01:29:20 +0200

    loglevel

--- dhis2/logger.py
+++ dhis2/logger.py
@@ -1,3 +1,4 @@
+import logging
 import logzero
 
 # DHIS2 logging format incl. [file:codeline]
@@ -7,15 +8,16 @@ log_format = '* %(levelname)1s  %(asctime)s,%(msecs)03d  %(message)s [%(module)s
 log_format_color = '%(color)s* %(levelname)1s%(end_color)s  %(asctime)s,%(msecs)03d  %(message)s [%(module)s:%(lineno)d]'  # noqa
 
 
-def setup_logger(logfile=None, backup_count=20):
+def setup_logger(logfile=None, backup_count=20, log_level=logging.INFO):
     """
     Setup logzero logger. if logfile is specified, create additional file logger
     :param logfile: path to log file destination
     :param backup_count: number of rotating files
+    :param log_level: min. log level FOR FILE LOGGING
     """
     formatter = logzero.LogFormatter(fmt=log_format_color, datefmt='%Y-%m-%d %H:%M:%S')
     logzero.setup_default_logger(formatter=formatter)
 
     if logfile:
         formatter = logzero.LogFormatter(fmt=log_format)
-        logzero.logfile(logfile, formatter=formatter, maxBytes=int(1e7), backupCount=backup_count)
+        logzero.logfile(logfile, formatter=formatter, loglevel=log_level, maxBytes=int(1e7), backupCount=backup_count)
