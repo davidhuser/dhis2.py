bc272560e72d8385a14be934854e9a0a7b73eb86
dhuser <dhuser@baosystems.com>
2018-07-10 12:17:15 +0200

    add logger 'test'

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,17 +1,9 @@
-""" TODO
-
-import os
-import tempfile
-import logzero
-
-
-def test_setup_logger_default(capsys):
+def test_setup_logger_default():
     from dhis2.logger import setup_logger
     from dhis2 import logger
     setup_logger()
-    logger.info("hello1")
+    logger.info("info")
+    logger.warn("warn")
+    logger.debug("debug")
+    logger.error("error")
 
-    captured = capsys.readouterr()
-    assert "hello1" in captured.out
-    assert "hello1" in captured.err
-"""
